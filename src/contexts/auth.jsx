d0a7e77af986a8e863d2c400ba77e35a2b4a6c06
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-02-21 14:18:41 -0300

    Ultimo commit dia 21/02/23

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect, createContext } from "react";
 
 import { useNavigate } from "react-router-dom";
 
+import { api, createSession } from "../services/api.js";
+
 export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => { 
@@ -13,34 +15,41 @@ export const AuthProvider = ({children}) => {
 
     useEffect(() => {
         const recoveredUser = localStorage.getItem('user');
+        const token = localStorage.getItem("token");
 
-        if(recoveredUser) {
+        if(recoveredUser && token) {
             setUser(JSON.parse(recoveredUser));
+            api.defaults.headers.Authorization = `Bearer ${token}`;
         }
 
         setLoading(false);
     }, []);
 
-    const login = (email, password) => {
-        console.log("login auth", {email, password});
+    const login = async (email, password) => {
+        const response = await createSession(email, password);
 
-        // ir na api e criar uma session
-        const loggedUser = {
-            id: '123',
-            email,
-        };
+        console.log("login", response.data);
+
+        const loggedUser = response.data.user;
+        const token = response.data.token;
 
         localStorage.setItem("user", JSON.stringify(loggedUser));
+        localStorage.setItem("token", token);
 
-        if(password === "secret") {
-            setUser(loggedUser);
-            navigate("/");
-        }        
-    };
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+
+        setUser(loggedUser);
+        navigate("/");
+        };
 
     const logout = () => {
         console.log("logout");
+        
         localStorage.removeItem("user");
+        localStorage.removeItem("token");
+
+        api.defaults.headers.Authorization = null;
+
         setUser(null);
         navigate("/login");
     };
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,20 +1,46 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 import { AuthContext } from "../../contexts/auth";
 
+import { getUsers } from "../../services/api";
+
 const HomePage = () => {
 
-    const { authenticated, logout } = useContext(AuthContext);
+    const { logout } = useContext(AuthContext);
+    const [ users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // não pode ser usado o async() no useEffect(), então cria-se 
+        // uma função anonima dentro do js
+        (async () => {
+            const response = await getUsers();
+            setUsers(response.data);
+            setLoading(false);
+        })();
+    }, []);
 
     const handleLogout = () => {
         logout();
+    };
+
+    if(loading) {
+        return <div className="loading">Carregando dados...</div>
     }
 
     return( 
         <> 
            <h1>HomePage</h1>
-           <p>{String(authenticated)}</p>
            <button onClick={handleLogout}>Logout</button>
+           <ul>
+            {
+                users.map((user) => (
+                    <li key ={user._id}>
+                        {user._id} - {user.email}
+                    </li>
+                ))
+            }
+           </ul>
         </>
     );
 };
